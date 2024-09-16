0f5f141952dc373725281b7fb3e21ed1f62cdc26
Mychael Walton <mychaelrw@gmail.com>
2024-09-16 16:34:37 -0700

    Adding api gateway iac

--- cmd/handlers/main.go
+++ cmd/handlers/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
+	"crypto/ed25519"
+	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/bsdlp/discord-interactions-go/interactions"
 )
 
 // APIKey is the API key used for authentication.
@@ -14,6 +17,13 @@ const API_KEY = "YOUR_API_KEY"
 func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	// Extract x-signature-timestamp and x-signature-ed25519 values from headers
 	// Concatenate x-signature-timestamp and request body
+
+	verified := interactions.Verify(r, ed25519.PublicKey(discordPubkey))
+
+	fmt.Println("Marshalling json: ")
+
+	jason_marsh, _ := json.Marshal(request)
+	fmt.Println(string(jason_marsh))
 
 	timestamp := request.Headers["x-signature-timestamp"]
 	signature := request.Headers["x-signature-ed25519"]
@@ -25,7 +35,7 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello, World!",
+		Body:       "He's GHET!",
 	}, nil
 }
 
--- go.mod
+++ go.mod
@@ -7,4 +7,7 @@ require (
 	github.com/kevinburke/nacl v0.0.0-20210405173606-cd9060f5f776
 )
 
-require filippo.io/edwards25519 v1.1.0 // indirect
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/bsdlp/discord-interactions-go v0.0.0-20201227083222-a2ba84473ce8 // indirect
+)
