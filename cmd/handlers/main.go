1521cb6262909dd1655bc914b50ff4977c87fc10
Mychael Walton <mychaelrw@gmail.com>
2024-09-17 09:08:33 -0700

    Happy path working

--- cmd/handlers/main.go
+++ cmd/handlers/main.go
@@ -1,44 +1,85 @@
 package main
 
 import (
-	"context"
+	"bytes"
 	"crypto/ed25519"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
 	"github.com/bsdlp/discord-interactions-go/interactions"
 )
 
-// APIKey is the API key used for authentication.
-const API_KEY = "YOUR_API_KEY"
-
-func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	// Extract x-signature-timestamp and x-signature-ed25519 values from headers
-	// Concatenate x-signature-timestamp and request body
-
-	verified := interactions.Verify(r, ed25519.PublicKey(discordPubkey))
-
-	fmt.Println("Marshalling json: ")
-
-	jason_marsh, _ := json.Marshal(request)
-	fmt.Println(string(jason_marsh))
-
-	timestamp := request.Headers["x-signature-timestamp"]
-	signature := request.Headers["x-signature-ed25519"]
-	bodyTime := timestamp + request.Body
-
-	fmt.Println("Timestamp: ", timestamp)
-	fmt.Println("Signature: ", signature)
-	fmt.Println("BodyTime: ", bodyTime)
-
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "He's GHET!",
-	}, nil
-}
-
 func main() {
-	lambda.Start(HandleRequest)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		// TODO: BEFORE COMMIT: HIDE THIS KEY
+		hexEncodedDiscordPubkey := "{INSERT_PUBLIC_KEY_HERE}"
+		discordPubkey, err := hex.DecodeString(hexEncodedDiscordPubkey)
+		if err != nil {
+			fmt.Printf("error decoding hex encoded discord pubkey: %v\n", err)
+		}
+		verified := interactions.Verify(r, ed25519.PublicKey(discordPubkey))
+		if !verified {
+			http.Error(w, "signature mismatch", http.StatusUnauthorized)
+			return
+		}
+
+		defer r.Body.Close()
+		var data interactions.Data
+		jsonErr := json.NewDecoder(r.Body).Decode(&data)
+		if jsonErr != nil {
+			fmt.Printf("error decoding request body: %v\n", jsonErr)
+		}
+
+		fmt.Println(data.Type)
+
+		// respond to ping
+		if data.Type == interactions.Ping {
+			_, err := w.Write([]byte(`{"type":1}`))
+			if err != nil {
+				fmt.Printf("error writing response: %v\n", err)
+			}
+			return
+		}
+
+		// handle command
+		response := &interactions.InteractionResponse{
+			Type: interactions.ChannelMessageWithSource,
+			Data: &interactions.InteractionApplicationCommandCallbackData{
+				Content: "he's ghet!",
+			},
+		}
+
+		var responsePayload bytes.Buffer
+		err = json.NewEncoder(&responsePayload).Encode(response)
+		if err != nil {
+			fmt.Printf("error encoding response: %v\n", err)
+		}
+
+		fmt.Println(data.ResponseURL())
+		fmt.Println()
+
+		fmt.Println(responsePayload.String())
+
+		resp, err := http.Post(data.ResponseURL(), "application/json", &responsePayload)
+		if err != nil {
+			fmt.Printf("error sending response: %v\n", err)
+		} else {
+			defer resp.Body.Close()
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Printf("error reading response body: %v\n", err)
+			} else {
+				fmt.Println(string(body))
+			}
+		}
+		io.WriteString(w, "success")
+	})
+
+	lambda.Start(httpadapter.New(http.DefaultServeMux).ProxyWithContext)
 }
--- go.mod
+++ go.mod
@@ -4,10 +4,10 @@ go 1.22.3
 
 require (
 	github.com/aws/aws-lambda-go v1.47.0
-	github.com/kevinburke/nacl v0.0.0-20210405173606-cd9060f5f776
+	github.com/bsdlp/discord-interactions-go v0.0.0-20201227083222-a2ba84473ce8
 )
 
 require (
-	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/bsdlp/discord-interactions-go v0.0.0-20201227083222-a2ba84473ce8 // indirect
+	github.com/awslabs/aws-lambda-go-api-proxy v0.16.2
+	github.com/stretchr/testify v1.8.4 // indirect
 )
